f8da6633e2a7e78683c4884187282e48e1e8837b
Elfina <94973185+Minjiaaa@users.noreply.github.com>
2022-10-22 13:59:02 -0700

    Time: 202 ms (81.30%), Space: 15.4 MB (85.91%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,22 +1,27 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = res = 0
-        for num in nums:
-            if count == 0:
-                res = num
+#vote
+#         count = res = 0
+#         for num in nums:
+#             if count == 0:
+#                 res = num
             
-            if num != res:
-                count -= 1
-            else:
-                count += 1
-        return res
+#             if num != res:
+#                 count -= 1
+#             else:
+#                 count += 1
+#         return res
 
 #HashMap
         
-#         cnt = {}
+        cnt = {}
+        res, maxCount = 0, 0
         
-#         for s in nums:
-#             cnt[s] = cnt.get(s, 0) + 1
-#             if cnt[s] > len(nums) // 2:
-#                 return s
+        for s in nums:
+            cnt[s] = cnt.get(s, 0) + 1
+            res = s if cnt[s] > maxCount else res
+            maxCount = max(cnt[s], maxCount)
+        return res
+            # if cnt[s] > len(nums) // 2:
+            #     return s
                 
