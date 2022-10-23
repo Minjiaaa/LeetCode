fe85b9ccb7fe655ee94323f101fc023ab102d556
Elfina <94973185+Minjiaaa@users.noreply.github.com>
2022-10-23 16:49:57 -0700

    Time: 1144 ms (69.87%), Space: 28.7 MB (10.82%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,30 +1,38 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        return self.getMax(nums, 0 , len(nums) - 1)
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        result = nums[0]
+        for i in range(1, len(nums)):
+            dp[i] = max(dp[i - 1] + nums[i], nums[i])
+            result = max(result, dp[i])
+        return result
+#2. Divide and Conquer
+#         return self.getMax(nums, 0 , len(nums) - 1)
     
-    def getMax(self, nums, left, right):
-        if left == right:
-            return nums[left]
+#     def getMax(self, nums, left, right):
+#         if left == right:
+#             return nums[left]
 
-        mid = left + (right - left) // 2
+#         mid = left + (right - left) // 2
 
-        leftMax = self.getMax(nums, left, mid)
-        rightMax = self.getMax(nums, mid + 1, right)
-        crossMax = self.CrossMax(nums, left, right)
+#         leftMax = self.getMax(nums, left, mid)
+#         rightMax = self.getMax(nums, mid + 1, right)
+#         crossMax = self.CrossMax(nums, left, right)
 
-        return max(leftMax, rightMax, crossMax)
+#         return max(leftMax, rightMax, crossMax)
 
-    def CrossMax(self, nums, left, right):
-        mid = left + (right - left) // 2 #取整数
-        leftSum = nums[mid] #这一点没明白
-        leftMax = leftSum
-        for i in range(mid - 1, left - 1, -1): #中间，递减，直到左边
-            leftSum = leftSum + nums[i]
-            leftMax = max(leftSum, leftMax)
+#     def CrossMax(self, nums, left, right):
+#         mid = left + (right - left) // 2 #取整数
+#         leftSum = nums[mid] #这一点没明白
+#         leftMax = leftSum
+#         for i in range(mid - 1, left - 1, -1): #中间，递减，直到左边
+#             leftSum = leftSum + nums[i]
+#             leftMax = max(leftSum, leftMax)
 
-        rightSum = nums[mid + 1]
-        rightMax = rightSum
-        for i in range(mid + 2, right + 1):
-            rightSum = rightSum + nums[i]
-            rightMax = max(rightSum, rightMax)
-        return leftMax + rightMax
+#         rightSum = nums[mid + 1]
+#         rightMax = rightSum
+#         for i in range(mid + 2, right + 1):
+#             rightSum = rightSum + nums[i]
+#             rightMax = max(rightSum, rightMax)
+#         return leftMax + rightMax
