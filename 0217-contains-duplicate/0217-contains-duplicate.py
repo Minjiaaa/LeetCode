ad233ea7d416bedb95b0041ede9f72a11a17d169
Elfina <94973185+Minjiaaa@users.noreply.github.com>
2022-10-22 17:55:32 -0700

    Time: 1249 ms (10.79%), Space: 26 MB (67.07%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,17 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+#         cnt = {}
+        
+#         for num in nums:
+#             cnt[num] = cnt.get(num, 0) + 1
+            
+#             if cnt[num] > 1:
+#                 return True
+        return len(nums) > len(set(nums))
+        #如何保证每一个和任何一个？
+        #用set计数
+        
+                
+        
+        
