bc18a782804ebab2581924c7b78e6466ba23b9b6
Elfina <94973185+Minjiaaa@users.noreply.github.com>
2022-11-10 15:25:58 -0800

    Time: 223 ms (97.58%), Space: 15 MB (38.07%) - LeetHub

---
+++ 0255-verify-preorder-sequence-in-binary-search-tree/0255-verify-preorder-sequence-in-binary-search-tree.py
@@ -0,0 +1,11 @@
+class Solution:
+    def verifyPreorder(self, preorder: List[int]) -> bool:
+        stack = []
+        new_min = float('-inf') #初始下限值
+        for i in range(len(preorder)):
+            if preorder[i] < new_min:
+                return False
+            while stack and preorder[i] > stack[-1]:
+                new_min = stack.pop()
+            stack.append(preorder[i])
+        return True
