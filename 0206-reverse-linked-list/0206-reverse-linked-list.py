c46d96f48085e41ff5dd912c64fc470507aabc46
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2022-11-12 11:43:16 -0800

    Time: 74 ms (38.39%), Space: 15.3 MB (99.64%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,17 +5,11 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode(0)
-        dummy.next = head
-        
-        while head != None and head.next != None:
-            hnext = head.next
-            dnext = dummy.next
-            #先分别设定两个节点，然后不要动原来的链表，先设定头指针，注意有些节点断开之后没有指针了。
-            dummy.next = hnext
-            head.next = hnext.next #需要使用head这个指针
-            hnext.next = dnext
-           
-        return dummy.next
-            
+        cur, prev = head, None
+        while cur:
+            tmp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = tmp
+        return prev#prev相当于头节点
         
