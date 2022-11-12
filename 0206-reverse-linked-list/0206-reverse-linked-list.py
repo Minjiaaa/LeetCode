364995eda10377495c14090a874e81e06b77c0f9
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2022-11-12 12:06:32 -0800

    Time: 39 ms (92.08%), Space: 20.4 MB (14.82%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,11 +5,13 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        cur, prev = head, None
-        while cur:
-            tmp = cur.next
-            cur.next = prev
-            prev = cur
-            cur = tmp
-        return prev#prev相当于头节点
+        def recur(cur, pre):
+            #Base Case and also the terminated condition
+            if not cur: #当cur为空
+                return pre
+            res = recur(cur.next, cur) #记录反转列表的头节点
+            # recursion
+            cur.next = pre #修改当前节点指向前面的节点，一直逼近base case
+            return res
+        return recur(head, None) 
         
