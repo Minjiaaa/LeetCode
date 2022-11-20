f7d1383df5d1c43db35e2e3f9db11d21c8dc9a3f
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2022-11-20 14:55:59 -0800

    Time: 384 ms (91.71%), Space: 14.6 MB (88.03%) - LeetHub

--- 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.py
+++ 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.py
@@ -1,18 +1,31 @@
 class Solution:
     def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
-        n = len(s)
-        table = {}
+        
         left = 0
+        cnt = {}
         maxLen = 0
-        for right in range(0, n):
-            table[s[right]] = table.get(s[right], 0) + 1
-            while left < right and len(table) > 2:
-                table[s[left]] -= 1
-                if table[s[left]] == 0:
-                    del table[s[left]]
+        for right in range(len(s)):
+            cnt[s[right]] = cnt.get(s[right], 0) + 1
+            while left < right and len(cnt) > 2:
+                cnt[s[left]] -= 1
+                
+                if cnt[s[left]] == 0:
+                    del cnt[s[left]]
                 left += 1
+            
             maxLen = max(maxLen, right - left + 1)
+        
         return maxLen
+    
+    # e, c, e, b, a
+    # right = 0, cnt = {'e': 1} maxlen = 1
+    # right = 1, cnt = {'e' : 1, 'c' : 1}, maxLen = 2
+    # right = 2, cnt = {'e' : 2, 'c' : 1}, maxLen = 3
+    # right = 3, cnt = {'e' : 2, 'c' : 1, 'b' : 1}, cnt = {'e' : 1, 'c' : 1, 'b' : 1} 
+    #             left = 1, cnt = {'e' : 1, 'c' : 0, 'b' : 1} -> cnt = {'e' : 1, 'b' : 1}
+    #             left = 2, maxLen = 3
+                
+    
     
         # maxSub = 0
         # for i in range(n):
