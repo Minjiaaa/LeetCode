fd4983ba686a9d1cf50927421b56054b8f657956
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2022-11-22 21:35:50 -0800

    Time: 1375 ms (78.74%), Space: 33.2 MB (69.86%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,7 +1,7 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         preSumModK = 0
-        table = {0 : -1}
+        table = {0 : -1} #1改成-1之后就通过了
         res = 0
         for i in range(len(nums)):
             preSumModK = (nums[i] + preSumModK) % k
