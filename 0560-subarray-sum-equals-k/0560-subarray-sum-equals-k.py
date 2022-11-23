7e532c074674d0c2fd51d454a2a54c5b6416d1a9
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2022-11-22 18:53:59 -0800

    Time: 863 ms (21.20%), Space: 16.6 MB (34.65%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -5,13 +5,12 @@ class Solution:
         preSum = 0
         for i in range(len(nums)):
             preSum += nums[i]
-            if preSum - k in table: 
-                cnt += table[preSum - k]
-            if preSum in table:
-                table[preSum] += 1
-            else:
-                table[preSum] = 1
-
+            target = preSum - k   #目标前缀和：使得 当前前缀和-目标前缀和=k
+            
+            if target in table:   #查看是否有我们的目标前缀和，如果有，将他出现的次数加入到count
+                cnt += table[target]
+            #避免k = 0的情况
+            table[preSum] = table.get(preSum, 0) + 1  #更新当前前缀和出现的次数
         return cnt
         #preSum[j] - preSum[i] == k
         # preSum = [0] * (len(nums) + 1)
