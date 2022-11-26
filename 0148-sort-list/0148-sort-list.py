3d99e39e62f0e34ebf7cc1874a6e2ba08a9653e8
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2022-11-25 23:49:46 -0800

    Time: 1518 ms (44.79%), Space: 36.4 MB (55.13%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.py
@@ -0,0 +1,42 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         slow = slow.next
+#         fast = 0
+#         #如何使用快慢指针找到链表中点 
+#         #如何定义右指针
+#         right 
+        h, length, intv = head, 0, 1
+        while h: h, length = h.next, length + 1
+        res = ListNode(0)
+        res.next = head
+        # merge the list in different intv.
+        while intv < length:
+            pre, h = res, res.next
+            while h:
+                # get the two merge head `h1`, `h2`
+                h1, i = h, intv
+                while i and h: h, i = h.next, i - 1
+                if i: break # no need to merge because the `h2` is None.
+                h2, i = h, intv
+                while i and h: h, i = h.next, i - 1
+                c1, c2 = intv, intv - i # the `c2`: length of `h2` can be small than the `intv`.
+                # merge the `h1` and `h2`.
+                while c1 and c2:
+                    if h1.val < h2.val: pre.next, h1, c1 = h1, h1.next, c1 - 1
+                    else: pre.next, h2, c2 = h2, h2.next, c2 - 1
+                    pre = pre.next
+                pre.next = h1 if c1 else h2
+                while c1 > 0 or c2 > 0: pre, c1, c2 = pre.next, c1 - 1, c2 - 1
+                pre.next = h 
+            intv *= 2
+        return res.next
+
+# 作者：jyd
+# 链接：https://leetcode.cn/problems/sort-list/solution/sort-list-gui-bing-pai-xu-lian-biao-by-jyd/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
