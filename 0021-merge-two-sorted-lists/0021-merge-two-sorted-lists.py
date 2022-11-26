16a793afe0d3f42eadd85b3969e2a7269d68b9db
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2022-11-25 23:34:32 -0800

    Time: 74 ms (45.46%), Space: 14 MB (33.32%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -8,12 +8,16 @@ class Solution:
 # 终止条件：当两个链表都为空时，表示我们对链表已合并完成。
 # 如何递归：我们判断 l1 和 l2 头结点哪个更小，然后较小结点的 next 指针指向其余结点的合并结果。（调用递归）
 
-        if not list2: return list1
-        if not list1: return list2
-        
+        if not list1 and not list2:
+            return None
+        elif not list1:
+            return list2
+        elif not list2:
+            return list1
         if list1.val <= list2.val:
-            list1.next = self.mergeTwoLists(list1.next, list2)
-            return list1 #为什么这里要返回？
+            node = list1 #这一步是干嘛
+            node.next = self.mergeTwoLists(list1.next, list2)
         else:
-            list2.next = self.mergeTwoLists(list1, list2.next)
-            return list2 #为什么这里要返回？
+            node = list2
+            node.next = self.mergeTwoLists(list1, list2.next)
+        return node #为什么这里要返回？
