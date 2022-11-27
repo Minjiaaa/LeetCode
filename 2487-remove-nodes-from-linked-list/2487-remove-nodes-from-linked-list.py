038ed07eb994bfd18d190906d561cfebc59d74ed
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2022-11-26 20:43:17 -0800

    Time: 2961 ms (20.00%), Space: 61.1 MB (20.00%) - LeetHub

---
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverse(self, head):
+        tail = None
+        while head:
+            nextNode = head.next
+            head.next = tail
+            tail = head
+            head = nextNode
+        return tail
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr = tail = self.reverse(head)
+        maxNode = curr
+        while curr is not None and curr.next is not None:
+            if curr.next.val < maxNode.val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+                maxNode = curr
+        return self.reverse(tail)
+    #有recursion和dfs的思路，但是不知道如何下手，感觉可以分成小问题，但是不知道如何进一步解决
+    #   while head is not None:
+    #   head = head.next
+        # find the target node and delete the node
+            
+        
