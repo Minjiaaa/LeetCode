021da1f193a71e0fcdf8a0131d98f1509aeac508
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2022-11-27 15:42:38 -0800

    Time: 33 ms (99.51%), Space: 13.9 MB (74.48%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
@@ -5,18 +5,31 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
+        if head is None or head.next is None:
+            return head
+        
+        dummy = ListNode(0)
         dummy.next = head
-        move = dummy
-        if head is None:
-            return None
-        while move.next is not None and move.next.next is not None:
-            if move.next.val == move.next.next.val:
-                movePre = move.next.val
-                while move.next is not None and move.next.val == movePre:
-                    move.next = move.next.next
-            else:
+        
+        prev = dummy
+        move = head
+        
+        #move.next
+        while move is not None and move.next is not None:
+            #no duplicates
+            if move.next and move.next.val != move.val:
+                prev = prev.next
                 move = move.next
-        return dummy.next
+                continue
             
+            #duplicates exist
+            while move.next and move.next.val == move.val:
+                move.next = move.next.next
+            move = move.next
+              
+            #remove the first duplicate
+            prev.next = prev.next.next
+            
+        return dummy.next
+
         
