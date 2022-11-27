1c4c0300ab0e4c186824fa42bf863aebbba35f0b
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2022-11-26 22:59:28 -0800

    Time: 159 ms (90.88%), Space: 15.2 MB (92.52%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,9 +1,7 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        #这道题很像238，
         allSum = 0
         preSum = 0
-        cnt = 0
         for i in range(len(nums)):
             allSum += nums[i]
         
@@ -11,4 +9,5 @@ class Solution:
             if preSum == allSum - preSum - nums[i]:
                 return i
             preSum += nums[i]
+        
         return -1
