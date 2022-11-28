7ef79c43275bd855ed3272054fa4ff4beed83758
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2022-11-28 12:40:07 -0800

    Time: 59 ms (75.26%), Space: 13.9 MB (56.94%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        #edge case
+        if x == 1:
+            return 1
+        #recursive 
+        left = 0
+        right = x
+        while left < right:
+            mid = (left + right + 1) >> 1 # // 2 ** 1
+            if mid <= x // mid:
+                left = mid
+            else:
+                right = mid - 1
+        return left
+            
+            
+            
+        
