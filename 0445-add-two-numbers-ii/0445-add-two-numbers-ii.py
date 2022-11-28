26015aa853b17a97f81bddc39c1ecb7d07589737
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2022-11-28 09:52:01 -0800

    Time: 143 ms (58.85%), Space: 13.9 MB (46.38%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
@@ -5,9 +5,9 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        #堆+循环
         st1 = []
         st2 = []
+        
         while l1:
             st1.append(l1.val)
             l1 = l1.next
@@ -15,23 +15,22 @@ class Solution:
         while l2:
             st2.append(l2.val)
             l2 = l2.next
-        
-        carry = 0 #定义进位
+            
         dummy = ListNode(0)
+        carry = 0
         
         while st1 or st2 or carry:
             adder1 = st1.pop() if st1 else 0
             adder2 = st2.pop() if st2 else 0
             sum_ = adder1 + adder2 + carry
             if sum_ >= 10:
-                carry = 1
                 sum_ -= 10
+                carry = 1
             else:
                 carry = 0
-            
             cur = ListNode(sum_)
+            
             cur.next = dummy.next
             dummy.next = cur
         
         return dummy.next
-        
