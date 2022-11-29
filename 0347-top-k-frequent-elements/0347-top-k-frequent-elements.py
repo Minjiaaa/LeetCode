f28eee6d3c3ccffa8f79e3c33f296b3b36d3065c
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2022-11-28 23:20:52 -0800

    Time: 257 ms (33.61%), Space: 18.6 MB (91.91%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,8 +1,19 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        cnt = {}
-        nums.sort()
-        for s in nums:
-            cnt[s] = cnt.get(s, 0) + 1
-        return sorted(cnt, key = cnt.get, reverse=True)[:k]
+        #O(1) time
+        if k == len(nums):
+            return nums
+        # 1.build hashmap: char and frequency
+        count = Counter(nums)
+        
+        # 2-3 build heap of top k frequent elements and convert it into an output array
+        uniqueNums = list(count.keys())
+        # turn the hashmap into a priotity queue (key, value) in order
+        pq = [(count[num], num) for num in uniqueNums[:k]]
+        heapq.heapify(pq)
+        for num in uniqueNums[k:]:
+            heapq.heappush(pq, (count[num], num)) # insert and adjust the order
+            heapq.heappop(pq)
+        
+        return [ele[1] for ele in pq]
         
