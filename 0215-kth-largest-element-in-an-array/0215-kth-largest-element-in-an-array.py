dc1f91d3658bad5e37ed701666276c85797bb8ca
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2022-11-28 16:51:48 -0800

    Time: 2552 ms (17.08%), Space: 129.7 MB (6.67%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,7 +1,21 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        #quick select, O(n)
+        pivot = nums[0]
+        left  = [l for l in nums if l < pivot]
+        equal = [e for e in nums if e == pivot]
+        right = [r for r in nums if r > pivot]
+        
+        if k <= len(right):
+            return self.findKthLargest(right, k)
+        elif (k - len(right)) <= len(equal):
+            return equal[0]
+        else:
+            return self.findKthLargest(left, k - len(right) - len(equal))
+    
+
         #bruteforce
-        n = len(nums)
-        nums.sort()
-        return nums[n-k]
+#         n = len(nums)
+#         nums.sort()
+#         return nums[n-k]
         
