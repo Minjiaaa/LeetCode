d514b298dc9045748c95819bfcc1b69bfcb0d7ba
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2022-12-03 14:53:12 -0800

    Time: 4051 ms (48.06%), Space: 14.1 MB (65.08%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -1,18 +1,31 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        diff = float('-inf')
+        diff = float('inf')
         nums.sort()
         for i in range(len(nums)):
-            lo = i + 1
-            hi = len(nums) - 1
-            while lo < hi:
-                sum3 = nums[i] + nums[lo] + nums[hi]
-                if abs(sum3 - target) < abs(diff):
-                    diff = target - sum3
-                if sum3 < target:
-                    lo += 1
-                else:
-                    hi -= 1
-                if diff == 0:
-                    break
+            for j in range(i + 1, len(nums)):
+                complement = target - nums[i] - nums[j]
+                hi = bisect_right(nums, complement, j + 1)
+                lo = hi - 1
+                if hi < len(nums) and abs(complement - nums[hi]) < abs(diff):
+                    diff = complement - nums[hi]
+                if lo > j and abs(complement - nums[lo]) < abs(diff): #
+                    diff = complement - nums[lo]
+            if diff == 0:
+                break
         return target - diff
+                
+        # Two Pointers
+        #     lo = i + 1
+        #     hi = len(nums) - 1
+        #     while lo < hi:
+        #         sum3 = nums[i] + nums[lo] + nums[hi]
+        #         if abs(sum3 - target) < abs(diff):
+        #             diff = target - sum3
+        #         if sum3 < target:
+        #             lo += 1
+        #         else:
+        #             hi -= 1
+        #         if diff == 0:
+        #             break
+        # return target - diff
