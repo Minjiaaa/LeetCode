001faa4dfd93f52d882aa8562aee51d931cea821
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2022-12-18 17:29:11 -0800

    Time: 67 ms (13.76%), Space: 13.9 MB (31.79%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -2,6 +2,7 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
             return []
+        
         path = []
         res = []
 
@@ -16,11 +17,14 @@ class Solution:
             '9': 'wxyz'
         }
         
+    #      a.       b      c
+    # d. e. f.    d e f         
+    
+        
         def helper(digits, index):
-            if index == len(digits):
+            if index == len(digits): #错误：index == len（digits）-1
                 res.append(path[:])
                 return res
-            
 #             for index in range(len(digits)):
 #                 letters = phone[digits[index]]
 
@@ -35,12 +39,10 @@ class Solution:
                 helper(digits, index + 1)
                 path.pop()
 
-        
         helper(digits, 0)
         
         for i, path in enumerate(res):
             res[i] = ''.join(path)
-
         
         return res
                     
