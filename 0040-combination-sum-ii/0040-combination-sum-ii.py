1cd43a2d25feb03f6e5c14a44eeccea07d35991a
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2022-12-19 13:34:09 -0800

    Time: 322 ms (10.50%), Space: 14 MB (61.27%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -7,7 +7,8 @@ class Solution:
         
         
         def helper(candidates, target, index):
-            # str_path = tuple(path)
+            # 使用tuple（hashable）,会超时，所以只能对每一个元素进行进行去重操作
+            # str_path = tuple(path) 
             if sum(path) > target: #or str_path in visit:
                 return
             
@@ -17,14 +18,18 @@ class Solution:
                 return
             
             for i in range(index, len(candidates)):
+            #不使用used数组进行去重的方式1
                 if i > index and candidates[i] == candidates[i-1]:
-                    continue
+                    continue 
+                    
+            #不使用used数组进行去重的方式2
                 # if i != 0 and candidates[i] == candidates[i-1] and not path:
                 #     continue
                 path.append(candidates[i])
                 helper(candidates, target, i + 1) # i+1 代表当前组内元素只选取一次
                 path.pop()
         
+        # 进行去重的方式3
         # for i, num in enumerate(candidates):
         #     if num in visit:
         #         continue
