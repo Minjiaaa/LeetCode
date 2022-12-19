f42832908bf2d76c35f870a4adb044f047a4e415
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2022-12-18 20:28:18 -0800

    Time: 45 ms (89.74%), Space: 15.1 MB (16.96%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -1,20 +1,27 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        ans = [0 for i in range(n)]
-        for i in range(1, n + 1):
-            if i % 3 == 0 and i % 5 == 0:
-                ans[i - 1] = "FizzBuzz"
-                #ans.append("FizzBuzz")
-            #elif i % 3 == 0 and i % 5 !=0 :
-          
-            elif i % 3 == 0:
-                #ans.append('Fizz')
-                ans[i - 1] = "Fizz"
-            elif i % 5 == 0: #and i % 3 != 0:
-                #ans.append('Buzz')
-                ans[i - 1] = "Buzz"
-            else:
-                ans[i - 1] = str(i)
+        ans = []
+        
+        fizz_buzz_dict = {3: 'Fizz', 5: 'Buzz'}
+        divisors = [3, 5]
+        
+        for num in range(1, n + 1):
+            
+            ans_str = [] #设置中间收集单一结果的变量
+            
+            # see whether the num is divisible by key, and add corresponding string map
+            for key in divisors:
+               
+                if num % key == 0:
+                    ans_str.append(fizz_buzz_dict[key]) 
+                
+            if not ans_str:
+                ans_str.append(str(num))
+
+            ans.append(''.join(ans_str))
+        
         return ans
+            
+            
             
         
