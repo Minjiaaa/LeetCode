199565d7cd08de2f069c034caf54046071ffc35c
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2022-12-18 17:19:55 -0800

    Time: 61 ms (33.13%), Space: 13.9 MB (80.06%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,50 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        path = []
+        res = []
+
+        phone = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+        
+        def helper(digits, index):
+            if index == len(digits):
+                res.append(path[:])
+                return res
+            
+#             for index in range(len(digits)):
+#                 letters = phone[digits[index]]
+
+#                 for letter in letters:
+#                     path.append(letter)
+#                     helper(digits, index + 1)
+#                     path.pop()
+            letters = phone[digits[index]]
+
+            for letter in letters:
+                path.append(letter)
+                helper(digits, index + 1)
+                path.pop()
+
+        
+        helper(digits, 0)
+        
+        for i, path in enumerate(res):
+            res[i] = ''.join(path)
+
+        
+        return res
+                    
+                
+            
+            
+        
