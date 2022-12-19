90173c5555113825916fb5e94a3e00527e370183
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2022-12-18 18:09:18 -0800

    Time: 50 ms (80.72%), Space: 15 MB (85.86%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -0,0 +1,20 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        ans = []
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                # ans[i] = "FizzBuzz"
+                ans.append("FizzBuzz")
+            #elif i % 3 == 0 and i % 5 !=0 :
+            #ans[i] = "Fizz"
+            elif i % 3 == 0:
+                ans.append('Fizz')
+            elif i % 5 == 0: #and i % 3 != 0:
+                ans.append('Buzz')
+            
+            else:
+                ans.append(str(i))
+            
+        return ans
+            
+        
