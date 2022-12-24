45a253e6da7f15b5608696e4d99f6d7f5da901a3
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2022-12-24 15:11:35 -0800

    Time: 108 ms (65.50%), Space: 14.5 MB (48.48%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,44 +1,62 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        if not n: return []
-        board = [['.'] * n for _ in range(n)]
+        board = [['.'] * n for i in range(n)]
         res = []
-        def isVaild(board,row, col):
-            #判断同一列是否冲突
+        
+        def isValid(board, row, col):
+            #同一列
             for i in range(len(board)):
-                if board[i][col] == 'Q':
+                if board[i][col] =='Q':
+                    #print('here?')
                     return False
-            # 判断左上角是否冲突
-            i = row -1
-            j = col -1
-            while i>=0 and j>=0:
-                if board[i][j] == 'Q':
+              # 判断左上角是否冲突
+# 相当于遍历了前面所有的方块，所以不能用for循环写。
+#             for i in range(row-1, -1, -1):
+#                 for j in range(col-1, -1, -1):
+#                     if board[i][j] == 'Q':
+#                         print('herere?')
+#                         return False
+            
+#             for i in range(row -1, -1, -1):
+#                 for j in range(col + 1, len(board)):
+#                     if board[i][j] == 'Q':
+#                         print('hererere?')
+#                         return False
+            i = row - 1
+            j = col - 1
+            while i >= 0 and j >= 0:
+                if board[i][j] == "Q":
                     return False
                 i -= 1
                 j -= 1
-            # 判断右上角是否冲突
+            
             i = row - 1
             j = col + 1
-            while i>=0 and j < len(board):
-                if board[i][j] == 'Q':
+            while i >= 0 and j < len(board):
+                if board[i][j] == "Q":
                     return False
                 i -= 1
                 j += 1
             return True
-
-        def backtracking(board, row, n):
-            # 如果走到最后一行，说明已经找到一个解
+        
+        def helper(board, row, n):
+            #print(row, n)
             if row == n:
                 temp_res = []
                 for temp in board:
-                    temp_str = "".join(temp)
-                    temp_res.append(temp_str)
-                res.append(temp_res)
+                    temp_str = ''.join(temp) #转换成字符串
+                    temp_res.append(temp_str) #对于每一个可能的结果进行形式上的处理
+                #print(temp_res)
+                res.append(temp_res) #将单一结果加入结果集
+                
             for col in range(n):
-                if not isVaild(board, row, col):
+                if not isValid(board, row, col):
                     continue
                 board[row][col] = 'Q'
-                backtracking(board, row+1, n)
+                helper(board, row + 1, n)
                 board[row][col] = '.'
-        backtracking(board, 0, n)
+                
+        
+        helper(board, 0, n)
         return res
+        
