6de73a75818960c7269c3c6359392d3fc8b5e391
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2022-12-26 14:51:46 -0800

    Time: 35 ms (91.73%), Space: 14.3 MB (51.61%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -24,20 +24,41 @@ class Solution:
 #                     queue.append(node.right)
                     
 #         return res
+#     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+#         if not root:
+#             return []
+
+#         queue = [root]
+#         res = []
+
+#         while queue:
+#             next_queue = []
+#             res.append([node.val for node in queue])
+#             for node in queue:
+#                 if node.left:
+#                     next_queue.append(node.left)
+#                 if node.right:
+#                     next_queue.append(node.right)
+#             queue = next_queue
+#         return res
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
 
-        queue = [root]
+        queue = deque([root, None])
         res = []
-
+        level = []
         while queue:
-            next_queue = []
-            res.append([node.val for node in queue])
-            for node in queue:
-                if node.left:
-                    next_queue.append(node.left)
-                if node.right:
-                    next_queue.append(node.right)
-            queue = next_queue
+            node = queue.popleft()
+            if node is None:
+                res.append(level)
+                level = []
+                if queue: #这里是啥意思？
+                    queue.append(None)
+                continue
+            level.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
         return res
