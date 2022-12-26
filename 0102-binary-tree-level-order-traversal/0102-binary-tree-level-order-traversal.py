2f461c13edc928daa8e48dd1c64e79f668086bf6
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2022-12-26 14:39:13 -0800

    Time: 41 ms (79.06%), Space: 14.2 MB (84.21%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -5,22 +5,39 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+#     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+#         if not root:
+#             return []
+#         res = []
+        
+#         # deque是双端队列，相当于是给deque传入了一个值为root的list
+#         queue = deque([root])
+        
+#         while queue:
+#             res.append([node.val for node in queue])
+            
+#             for i in range(len(queue)):
+#                 node = queue.popleft()
+#                 if node.left:
+#                     queue.append(node.left)
+#                 if node.right:
+#                     queue.append(node.right)
+                    
+#         return res
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
+
+        queue = [root]
         res = []
-        
-        # deque是双端队列，相当于是给deque传入了一个值为root的list
-        queue = deque([root])
-        
+
         while queue:
+            next_queue = []
             res.append([node.val for node in queue])
-            
-            for i in range(len(queue)):
-                node = queue.popleft()
+            for node in queue:
                 if node.left:
-                    queue.append(node.left)
+                    next_queue.append(node.left)
                 if node.right:
-                    queue.append(node.right)
-                    
+                    next_queue.append(node.right)
+            queue = next_queue
         return res
