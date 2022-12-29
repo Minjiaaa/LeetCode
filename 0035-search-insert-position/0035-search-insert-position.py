7c1cf73bab1f0c74b0ae9f097058c014aa5839fe
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2022-12-29 15:54:52 -0800

    Time: 60 ms (69.85%), Space: 14.6 MB (99.06%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,13 +1,14 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums) - 1
-        
+        l = 0
+        r = len(nums) - 1
         while l <= r:
-            mid = (r -l) // 2 + l
-            if target > nums[mid]:
-                l = mid + 1
+            m = (r + l) >> 1
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                l = m + 1
             else:
-                r = mid - 1
+                r = m - 1
+        
         return l
-        #不知道应该怎么找到那个数
-                    
