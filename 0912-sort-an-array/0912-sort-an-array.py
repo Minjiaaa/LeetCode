c70aae6013a0827a0344858c2d11833d9ae43909
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2022-12-30 23:14:04 -0800

    Time: 1957 ms (68.76%), Space: 21.7 MB (83.11%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,3 +1,41 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        return sorted(nums)
+        # return sorted(nums)
+        # partition part
+        if len(nums) <= 1:
+            return nums
+        
+        m = len(nums) // 2
+        left = nums[ :m]
+        right = nums[m: ]
+        
+        # recursively
+        self.sortArray(left)
+        self.sortArray(right)
+        
+        
+        i = j = k = 0
+        
+        # merge 
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                nums[k] = left[i]
+                i += 1
+            else:
+                nums[k] = right[j]
+                j += 1
+            k += 1
+        
+        
+        while i < len(left):
+            nums[k] = left[i]
+            i += 1
+            k += 1
+            
+        while j < len(right):
+            nums[k] = right[j]
+            j += 1
+            k += 1
+        
+        return nums
+                
