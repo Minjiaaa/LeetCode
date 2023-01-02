4dc68b8f9465a70ff809e8234ebb9ceb9901a343
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2023-01-02 13:27:04 -0800

    Time: 655 ms (80.09%), Space: 14 MB (31.54%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -3,24 +3,13 @@ class Solution:
         if not s:
             return ""
         
-        start, longest = 0, 0
-        for middle in range(len(s)):
-            # add
-            left, right = middle, middle
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                left -= 1
-                right += 1
-            if longest < right - left - 1:
-                longest = right - left - 1 # right = left + 1 - 2
-                start = left + 1
-            
-            left, right = middle, middle + 1
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                left -= 1
-                right += 1
-            if longest < right - left - 1:
-                longest = right - left - 1 # right = left + 1 - 2
-                start = left + 1
-            
-        return s[start: start + longest]
-            
+        res = (0, 0)
+        for mid in range(len(s)):
+            res = max(res, self.get_palindrome_from(s, mid, mid))
+            res = max(res, self.get_palindrome_from(s, mid, mid + 1))
+        return s[res[1]: res[0] + res[1]]
+    def get_palindrome_from(self, s, left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return right - left - 1, left + 1
