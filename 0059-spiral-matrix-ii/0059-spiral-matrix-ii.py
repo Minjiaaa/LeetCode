20946e2db08a4dc7a2a48daf6777adbc5c773c31
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2023-01-01 20:59:09 -0800

    Time: 34 ms (86.45%), Space: 13.8 MB (82.44%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
@@ -1,26 +1,29 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
         nums = [[0] * n for _ in range(n)]
-        startx, starty = 0, 0               # 起始点
-        loop, mid = n // 2, n // 2          # 迭代次数、n为奇数时，矩阵的中心点
-        count = 1                           # 计数
-
-        for offset in range(1, loop + 1) :      # 每循环一层偏移量加1，偏移量从1开始
-            for i in range(starty, n - offset) :    # 从左至右，左闭右开
-                nums[startx][i] = count
-                count += 1
-            for i in range(startx, n - offset) :    # 从上至下
-                nums[i][n - offset] = count
-                count += 1
-            for i in range(n - offset, starty, -1) : # 从右至左
-                nums[n - offset][i] = count
-                count += 1
-            for i in range(n - offset, startx, -1) : # 从下至上
-                nums[i][starty] = count
-                count += 1                
-            startx += 1         # 更新起始点
-            starty += 1
-
-        if n % 2 != 0 :			# n为奇数时，填充中心点
-            nums[mid][mid] = count 
+        start_x, start_y = 0, 0
+        loop = n // 2
+        mid = n // 2
+        num = 1
+        
+        for offset in range(1, loop + 1):
+            # 从左到右
+            for j in range(start_y, n - offset):
+                nums[start_x][j] = num
+                num += 1
+            for i in range(start_x, n - offset):
+                nums[i][n - offset] = num
+                num += 1
+            for j in range(n - offset, start_y, -1):
+                nums[n - offset][j] = num
+                num += 1
+            for i in range(n - offset, start_x, -1):
+                nums[i][start_y] = num
+                num += 1
+            start_x += 1
+            start_y += 1
+        
+        if n % 2 == 1:
+            nums[mid][mid] = num
+        
         return nums
