5ad961aa5430436e5cd7a1f89d61f4bff76b0aa3
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2023-01-01 19:24:34 -0800

    Time: 265 ms (87.92%), Space: 27.2 MB (10.50%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -1,12 +1,14 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        res = float("inf")
-        sum_ = 0
+        res = float("inf") # 要求最小值，起始值赋一个最大值
         i = 0
+        cur_sum = 0
+        # 枚举法
         for j in range(len(nums)):
-            sum_ += nums[j]
-            while sum_ >= target:
-                res = min(res, j - i + 1)
-                sum_ -= nums[i]
-                i += 1
+            cur_sum += nums[j]
+            while cur_sum >= target:
+                    res = min(res, j - i + 1)
+                    cur_sum -= nums[i]
+                    i += 1 # 往后找是否还有符合的元素
         return 0 if res == float("inf") else res
+                    
