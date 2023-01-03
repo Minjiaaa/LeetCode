19f877deeb145e8942378bb0a045f748c657c896
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2023-01-02 17:59:24 -0800

    Time: 68 ms (11.13%), Space: 14 MB (17.11%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,13 +1,36 @@
 class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        if not haystack:
+    def strStr(self, hay: str, nee: str) -> int:
+#         if not haystack:
+#             return 0
+        
+#         for i in range(len(haystack) - len(needle) + 1): # haystack == needle
+#             for j in range(len(needle)):
+#                 if haystack[i + j] != needle[j]:
+#                     break
+#             else: # break和else只执行一个
+#                 return i
+        
+#         return -1 
+        if not hay:
             return 0
         
-        for i in range(len(haystack) - len(needle) + 1): # haystack == needle
-            for j in range(len(needle)):
-                if haystack[i + j] != needle[j]:
-                    break
-            else: # break和else只执行一个
-                return i
+        power = 10 ** 8
+        hash_nee = 0
+        size = len(nee)
+        hash_hay = 0
+        
+        for n in nee:
+            hash_nee = (hash_nee * 31 + ord(n)) % power
+            
+        for i in range(len(hay)):
+            hash_hay = (hash_hay * 31 + ord(hay[i])) % power
+            if i < size - 1:
+                continue
+            if hash_hay == hash_nee:
+                return i - size + 1
+            hash_hay = (hash_hay -  ord(hay[i-size+1]) * (31 ** (size-1)) ) % power
+            
+            if hash_hay < 0:
+                hash_hay += power
         
         return -1
