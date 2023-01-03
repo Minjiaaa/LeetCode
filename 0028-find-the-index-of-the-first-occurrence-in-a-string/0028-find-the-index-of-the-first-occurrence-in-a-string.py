63ac9724a1dcd8dbe81fb2c485930e0b49d9e732
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2023-01-02 17:12:46 -0800

    Time: 59 ms (32.35%), Space: 13.9 MB (17.11%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -3,11 +3,11 @@ class Solution:
         if not haystack:
             return 0
         
-        for i in range(len(haystack) - len(needle) + 1):
+        for i in range(len(haystack) - len(needle) + 1): # haystack == needle
             for j in range(len(needle)):
                 if haystack[i + j] != needle[j]:
                     break
-            else:
+            else: # break和else只执行一个
                 return i
         
         return -1
